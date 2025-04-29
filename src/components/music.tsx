e6b25ff8843efcf56a0dfa39400bc5de3fbdca00
Dnmrk4 <mutaidanmark@gmail.com>
2025-04-29 05:50:31 +0300

    added button

--- src/components/music.tsx
+++ src/components/music.tsx
@@ -1,13 +1,51 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
 function Music() {
+  const [showIframe, setShowIframe] = useState(false);
+
+  const handleClick = (
+    e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>
+  ) => {
+    e.preventDefault(); // Prevent default link behavior
+    setShowIframe(!showIframe); // Toggle iframe visibility
+  };
+
   return (
     <>
       <div className="flex flex-col max-w-screen w-full overflow-hidden justify-center mx-auto ">
+        {!showIframe ? (
+          <a href="#" onClick={handleClick}>
+            <p className="text-sm p-7 m-3 flowty animate-pulse text-center text-white drop-shadow-[0_0_10px_rgba(253,224,71,0.8)]">
+              Listen to my latest Hit!! 🔥
+            </p>
+          </a>
+        ) : (
+          <button
+            onClick={handleClick}
+            className="mx-auto mt-12 p-2 w-10 h-10 flex items-center justify-center rounded-full bg-neutral-500 text-white hover:bg-neutral-600"
+          >
+            X
+          </button>
+        )}
+
+        {/* Conditionally render the iframe */}
+        {showIframe && (
+          <div className="mt-4 flex justify-center">
+            <iframe
+              src="https://www.boomplay.com/embed/202990835/MUSIC?colType=2&colID=76730240"
+              frameBorder="0"
+              className="rounded-md w-full h-75 md:h-96"
+              allowFullScreen
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="eager"
+            ></iframe>
+          </div>
+        )}
+
         <h2 className="text-3xl text-center p-3 flowty mx-auto ">Music</h2>
         <div className="flex flex-row md:flex-wrap  mx-auto justify-center  w-full overflow-hidden scroll-auto scrollbar-hide">
           {/* Card 1 */}
@@ -98,4 +136,5 @@ function Music() {
     </>
   );
 }
+
 export default Music;
